c27577bac14842da981dd343d323b4b86794f955
sethscheib <sethscheib@gmail.com>
2022-08-01 15:26:10 -0400

    added scirpt for publish to shared drive

---
+++ publishChanges.bat
@@ -0,0 +1,3 @@
+xcopy  /y "C:\Users\sscheib\OneDrive - Tooling Systems Group\scripts\networkSearch\designLaunch.bat" "S:\sscheib\test\"
+xcopy  /y "C:\Users\sscheib\OneDrive - Tooling Systems Group\scripts\networkSearch\quoteLaunch.bat" "S:\sscheib\test\"
+
