be534ce024a6d8a303104922831ee0e3bf09fc19
sethscheib <sethscheib@gmail.com>
2022-08-08 10:52:11 -0400

    updated path

--- publishChanges.bat
+++ publishChanges.bat
@@ -1,3 +1,2 @@
-xcopy  /y "C:\Users\sscheib\OneDrive - Tooling Systems Group\scripts\networkSearch\designLaunch.bat" "S:\sscheib\test\"
-xcopy  /y "C:\Users\sscheib\OneDrive - Tooling Systems Group\scripts\networkSearch\quoteLaunch.bat" "S:\sscheib\test\"
-
+xcopy  /y "C:\Users\sscheib\OneDrive - Tooling Systems Group\scripts\networkSearch\designLaunch.bat" "S:\sscheib\"
+xcopy  /y "C:\Users\sscheib\OneDrive - Tooling Systems Group\scripts\networkSearch\quoteLaunch.bat" "S:\sscheib\"
