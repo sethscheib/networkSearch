fbd64120577d60d5300409338cc00064b8e9391f
sethscheib <sethscheib@gmail.com>
2023-01-17 08:02:29 -0500

    update on quoteLaunch for year

--- quoteLaunch.bat
+++ quoteLaunch.bat
@@ -19,14 +19,14 @@ set /p varnum="Quote Number: "
 for /f "delims=" %%a in ('dir /b /ad /on "\\sts2.local\DFS\Quotes\CUSTOMER\%varcust%*"') do set customerfolder=%%a
 
 ::Set auto completed path
-set "pth=Q:\CUSTOMER\%customerfolder%\2022 Quotes\"
+set "pth=Q:\CUSTOMER\%customerfolder%\2023 Quotes\"
 
 ::search directory using quote number as a wildcard
-for /f "delims=" %%a in ('dir /b /ad /on "\\sts2.local\DFS\Quotes\CUSTOMER\%customerfolder%\2022 Quotes\%varnum%*"') do set quotefolder=%%a
+for /f "delims=" %%a in ('dir /b /ad /on "\\sts2.local\DFS\Quotes\CUSTOMER\%customerfolder%\2023 Quotes\%varnum%*"') do set quotefolder=%%a
 
 
 ::Set auto completed path
-set "pth=Q:\CUSTOMER\%customerfolder%\2022 Quotes\%quotefolder%\"
+set "pth=Q:\CUSTOMER\%customerfolder%\2023 Quotes\%quotefolder%\"
 
 ::Cleanup path string working in escape characters
 ::& => ^&
